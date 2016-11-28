b301ce2ebfdfb3c4a841765c43bec3e72ebe6dc9
Sandziuk <polutkanius@gmail.com>
2016-11-28 20:19:32 +0100

    First versions of PL_signs and BeepNotes

---
+++ Beeping_Notes.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <Windows.h>
+
+#define C0(x) Beep(16, x);
+#define Cis0(x) Beep(17, x);
+#define D0(x) Beep(18, x);
+#define Dis0(x) Beep(19, x);
+
+#define C2(x) Beep(65, x);
+#define Cis2(x) Beep(69, x);
+#define D2(x) Beep(73, x);
+#define Dis2(x) Beep(78, x);
+#define E2(x) Beep(82, x);
+#define F2(x) Beep(87, x);
+#define Fis2(x) Beep(93, x);
+#define G2(x) Beep(98, x);
+#define Gis2(x) Beep(104, x);
+#define A2(x) Beep(110, x);
+#define Ais2(x) Beep(117, x);
+#define B2(x) Beep(123, x);
+
+#define C3(x) Beep(131, x);
+#define Cis3(x) Beep(189, x);
+#define D3(x) Beep(147, x);
+#define Dis3(x) Beep(156, x);
+#define E3(x) Beep(165, x);
+#define F3(x) Beep(175, x);
+#define Fis3(x) Beep(185, x);
+#define G3(x) Beep(196, x);
+#define Gis3(x) Beep(208, x);
+#define A3(x) Beep(220, x);
+#define Ais3(x) Beep(233, x);
+#define B3(x) Beep(247, x);
+
+#define C4(x) Beep(262, x);
+#define Cis4(x) Beep(277, x);
+#define D4(x) Beep(294, x);
+#define Dis4(x) Beep(311, x);
+#define E4(x) Beep(330, x);
+#define F4(x) Beep(349, x);
+#define Fis4(x) Beep(370, x);
+#define G4(x) Beep(392, x);
+#define Gis4(x) Beep(415, x);
+#define A4(x) Beep(440, x);
+#define Ais4(x) Beep(466, x);
+#define B4(x) Beep(494, x);
+
+#define C5(x) Beep(523, x);
+#define Cis5(x) Beep(554, x);
+#define D5(x) Beep(587, x);
+#define Dis5(x) Beep(622, x);
+#define E5(x) Beep(659, x);
+#define F5(x) Beep(698, x);
+#define Fis5(x) Beep(740, x);
+#define G5(x) Beep(784, x);
+#define Gis5(x) Beep(831, x);
+#define A5(x) Beep(880, x);
+#define Ais5(x) Beep(932, x);
+#define B5(x) Beep(988, x);
+
+#define C6(x) Beep(1047, x);
+#define Cis6(x) Beep(1109, x);
+#define D6(x) Beep(1175, x);
+#define Dis6(x) Beep(1245, x);
+#define E6(x) Beep(1319, x);
+#define F6(x) Beep(1397, x);
+#define Fis6(x) Beep(1480, x);
+#define G6(x) Beep(1568, x);
+#define Gis6(x) Beep(1661, x);
+#define A6(x) Beep(1760, x);
+#define Ais6(x) Beep(1865, x);
+#define B6(x) Beep(1976, x);
---
+++ PL_sings.h
@@ -0,0 +1,17 @@
+//This header file is thought to be of some use when trying to include polish diacritic signs in written text 
+//in use with the iostream library.
+
+#ifndef PL_signs
+#define PL_signs
+
+#define ae <<(unsigned char)165
+#define ce <<(unsigned char)134
+#define ee <<(unsigned char)169
+#define le <<(unsigned char)136
+#define ne <<(unsigned char)228
+#define oe <<(unsigned char)162
+#define se <<(unsigned char)152
+#define ze <<(unsigned char)190
+#define zie <<(unsigned char)171
+
+#endif
